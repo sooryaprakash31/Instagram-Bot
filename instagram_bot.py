1eb4cfc339e793cbcf27a63d622944c947e3396b
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-21 21:51:39 +0530

    Skips all the dialogs and logs out from account

--- instagram_bot.py
+++ instagram_bot.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import os
+from configparser import ConfigParser
 
 class InstagramBot:
 
@@ -18,13 +19,25 @@ class InstagramBot:
           self.driver.get("https://www.instagram.com/accounts/login/")
           sleep(1)
 
-          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')[0].send_keys("hello")
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')[0].send_keys(self.username)
           sleep(1)
-          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')[0].send_keys("hello")
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')[0].send_keys(self.password)
           sleep(2)
           self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button')[0].click()
-
-
+          sleep(2)
+          #self.driver.find_elements_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+          self.driver.get("https://www.instagram.com/{}/".format(self.username))
+          sleep(2)
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/button')[0].click()
+          sleep(3)
+          self.driver.find_elements_by_xpath('/html/body/div[4]/div/div/div/div/button[9]')[0].click()
+          sleep(5)
+          self.driver.quit()
 if __name__ == '__main__':
 
-     bot = InstagramBot()
+     config = ConfigParser()
+     config.read('config.ini')
+
+     username = config.get('AUTH', 'USERNAME')
+     password = config.get('AUTH', 'PASSWORD')
+     bot = InstagramBot(username,password)
