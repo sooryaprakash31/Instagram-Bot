34f7240722bf35cea34df4ecc6b977092634566d
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-21 21:38:41 +0530

    Inputs username ,password and clicks the log in button

--- instagram_bot.py
+++ instagram_bot.py
@@ -15,8 +15,14 @@ class InstagramBot:
           #Create a new profile and get the paths using chrome://version/
           self.options.add_argument("user-data-dir=/home/soorya/.config/chromium/Profile 1")
           self.driver = webdriver.Chrome(options=self.options,executable_path = "/usr/lib/chromium-browser/chromedriver")
-          self.driver.get("https://instagram.com/")
+          self.driver.get("https://www.instagram.com/accounts/login/")
           sleep(1)
+
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')[0].send_keys("hello")
+          sleep(1)
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')[0].send_keys("hello")
+          sleep(2)
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button')[0].click()
 
 
 if __name__ == '__main__':
