1a102f74fc6cf59b1a91e5d3511625019980ccef
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-06-22 12:53:16 +0530

    Added follow-suggested function

--- instagram_bot.py
+++ instagram_bot.py
@@ -18,7 +18,26 @@ class InstagramBot:
           self.driver = webdriver.Chrome(options=self.options,executable_path = "/usr/lib/chromium-browser/chromedriver")
           self.driver.get("https://www.instagram.com/accounts/login/")
           sleep(1)
-          #username and password
+          #login
+          self.login(username,password)
+          sleep(10)
+          #Profile page to skip all the unwanted dialogs
+          #self.driver.get("https://www.instagram.com/{}/".format(self.username))
+          #sleep(3)
+          #home
+          #self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[1]/div')[0].click()
+          sleep(6)
+          #following the suggested accounts in main page
+          self.follow_suggested()
+          sleep(2)
+          #print(len(self.driver.find_elements_by_xpath("//*[text()='{}']".format("Follow"))))
+          #self.follow_suggested()
+          sleep(3)
+          #To log out
+          self.logout()
+
+     def login(self,username,password):
+          #input username and password
           self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')[0].send_keys(self.username)
           sleep(1)
           self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')[0].send_keys(self.password)
@@ -26,11 +45,18 @@ class InstagramBot:
           #log in button
           self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button')[0].click()
           sleep(2)
-          
-          #Profile page to skip all the unwanted dialogs
-          self.driver.get("https://www.instagram.com/{}/".format(self.username))
+     def follow_suggested(self):
+          self.driver.get("https://www.instagram.com/")
+          #see all button in suggested for you
+          self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[2]/div[1]/a')[0].click()
           sleep(2)
-          
+          #getting the suggesstions list
+          suggested_accounts_list = self.driver.find_elements_by_xpath("//*[text()='{}']".format("Follow"))
+          suggested_accounts_list[0].click()
+
+     def logout(self):
+          self.driver.get("https://www.instagram.com/{}/".format(self.username))
+          sleep(3)
           #settings in profile
           self.driver.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/button')[0].click()
           sleep(3)
@@ -38,6 +64,7 @@ class InstagramBot:
           self.driver.find_elements_by_xpath('/html/body/div[4]/div/div/div/div/button[9]')[0].click()
           sleep(5)
           self.driver.quit()
+
 if __name__ == '__main__':
 
      config = ConfigParser()
